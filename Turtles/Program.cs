c77abcf1a352a033f437e108035f4c8e12a09e0f
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-19 21:24:16 -0600

    commit1

--- Turtles/Program.cs
+++ Turtles/Program.cs
@@ -7,7 +7,10 @@ namespace Turtles
         public static void Main()
         {
             // TODO: Prints "I like turtles." 1000 times.
-
+            for (int turtles = 0; turtles < 1000; turtles++)
+            {
+                Console.WriteLine("I like turtles.");
+            }
             Console.ReadLine();
         }
     }
